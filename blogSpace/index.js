221bb3b9f801fccd53d6412a48be8d0bc3246179
iAmSoloDev <107878651+iAmSoloDev@users.noreply.github.com>
2024-02-11 17:01:44 +0000

    modify files in blogSpace folder

--- blogSpace/index.js
+++ blogSpace/index.js
@@ -1,23 +1,66 @@
 const title = document.getElementById('title')
 const body = document.getElementById('body')
 const blog = document.getElementById('blog')
-console.log(body);
+const form = document.querySelector('form')
+
+let postArray = []
+
+function renderPost() {
+    let html =''
+    
+    postArray.forEach(post => {
+        html += `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <hr>
+        `          
+    });
+    blog.innerHTML = html
+}
 
 
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(res => res.json())
 .then(data => {
-    const posts = data.slice(0,5)
-    let html =''
-    
-    posts.forEach(post => {
-        html += `
-                <h2>${post.title}</h2>
-                <p>${post.body}</p>
-                <hr>
-            `          
-        });
-        blog.innerHTML = html
-    
+    postArray = data.slice(0,3)
+    renderPost()
+})
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const postTitle = title.value
+    const postBody = body.value
+    const post = {
+        title: postTitle,
+        body:  postBody
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
     })
+    .then(res => res.json())
+    .then(data => {
+        postArray.unshift(data)
+        renderPost()
+    })
+
+    form.reset()
+})
+
+// fetch example with itunes API
+// fetch('https://itunes.apple.com/search?term=jack+johnson')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+    
